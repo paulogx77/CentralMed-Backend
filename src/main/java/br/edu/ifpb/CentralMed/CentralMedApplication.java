4469b02802ad570147bb21e3dcc1fd89dc9a0a17
paulogx77 <paulo.guilhermeez@gmail.com>
2026-01-03 21:42:33 -0300

    initial commit

---
+++ src/main/java/br/edu/ifpb/CentralMed/CentralMedApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.CentralMed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentralMedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CentralMedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/ifpb/CentralMed/CentralMedApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.CentralMed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentralMedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
